a9f1e1620e4fa65b247715976cdc67a576477dc2
Anderson <andersonswaimboyd@gmail.com>
2019-05-29 17:29:36 -0500

    added pseudocode to readme, added comments to game.js

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Word-Guess-Game
+
+This is where I am pseudocoding the js file:
+
+first off we want the different options to show as:
+        random word (spaces->letters) "answer"
+        user guess (key event)
+        discard box for wrong letters
+
+the computer should choose a random word from a
+pre-compiled list of words. while there are letters 
+left in the random word, the computer should compare 
+the user's inputted key to the random word chosen.
+
+if the user guesses any letters in the random word, 
+the blank spaces where the answer is should fill
+in with that letter. if there user guesses an incorrect 
+letter, it should be shown in a discard box below or 
+to the side of the box that shows the user's input.
+
+there needs to be a counter for wins and losses. 
--- javascript/game.js
+++ javascript/game.js
@@ -1,27 +1,37 @@
+//Defines our arrays
 var words = ["dog", "cat", "parrot", "goldfish", "hamster", "rabbit"];
+var wrong = [];
+var answer = [];
+
+//defines random word from words array
 var randomWord = words[Math.floor(Math.random()*words.length)];
+
+//where we want our game todisplay
 var userGuess = document.getElementById("guess");
 var answerBox = document.getElementById("answer");
 var wrongBox = document.getElementById("wrongGuess");
-var wrong = [];
-var answer = [];
+
+//adds spaces to answer array
 for(var x = 0; x < randomWord.length; x++) {
     answer[x] = "_";
 }
 
-while(randomWord.length > 0){
-    document.onkeyup = function(event){
-        var guess = event.key;
-        answerBox.textContent = answer;
-        userGuess.textContent = event.key;
-        for(var y = 0; y < randomWord.length; y++) {
-            if(randomWord[y] === guess){
-                answer[y] = guess;
-                remains--;
+var remains = randomWord.length(); // makes a variable the length of the random word
+
+//guessing game itself (this needs some work)
+document.onkeyup = function(event){//this makes the keys work (I think)
+    while(remains > 0){//this should mean "while random word length variable is greater than zero..."
+        var guess = event.key; //makes the key event a variable called guess
+        answerBox.textContent = answer.join(" "); //I want the answer to show
+        userGuess.textContent = event.key;//I want the key pressed to show
+        for(var y = 0; y < randomWord.length; y++) {//for as long as y is less than the length of the random word...
+            if(guess === randomWord[y]){//if the key pressed is one of the letters in the random word
+                answer[y] = guess;//the letter should show up in the answer
+                remains--;//the random word length variable defined earlier drops by 1
             }
             else{
-                wrong.push(guess);
-                wrongBox.textContent = wrong;
+                wrong.push(guess); //adds the key press to the array called wrong
+                wrongBox.textContent = wrong;//I want the wrong answers to show
             }
         }
     }
