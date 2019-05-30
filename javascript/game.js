8c3a807d68a381477f22fc397a1ddda1c3d1c10f
Anderson <andersonswaimboyd@gmail.com>
2019-05-29 23:20:53 -0500

    rewriting game.js, only able to show blanks and key event so far

--- javascript/game.js
+++ javascript/game.js
@@ -1,38 +1,30 @@
 //Defines our arrays
 var words = ["dog", "cat", "parrot", "goldfish", "hamster", "rabbit"];
-var wrong = [];
+var wrongLetters = [];
 var answer = [];
 
 //defines random word from words array
 var randomWord = words[Math.floor(Math.random()*words.length)];
+var splits = randomWord.split("");
+
+var blanks = "_ ";
+var displayWord = blanks.repeat(randomWord.length);
 
 //where we want our game todisplay
 var userGuess = document.getElementById("guess");
 var answerBox = document.getElementById("answer");
 var wrongBox = document.getElementById("wrongGuess");
+var display = document.getElementById("displayBlank");
 
-//adds spaces to answer array
-for(var x = 0; x < randomWord.length; x++) {
-    answer[x] = "_";
+display.textContent = displayWord;
+
+//guessing game itself (we need to compare the user input to the random array)
+document.onkeydown = function(event){
+    userGuess.textContent = event.key;
+    wrongLetters.push(event.key);
 }
-
-var remains = randomWord.length(); // makes a variable the length of the random word
-
-//guessing game itself (this needs some work)
-document.onkeyup = function(event){//this makes the keys work (I think)
-    while(remains > 0){//this should mean "while random word length variable is greater than zero..."
-        var guess = event.key; //makes the key event a variable called guess
-        answerBox.textContent = answer.join(" "); //I want the answer to show
-        userGuess.textContent = event.key;//I want the key pressed to show
-        for(var y = 0; y < randomWord.length; y++) {//for as long as y is less than the length of the random word...
-            if(guess === randomWord[y]){//if the key pressed is one of the letters in the random word
-                answer[y] = guess;//the letter should show up in the answer
-                remains--;//the random word length variable defined earlier drops by 1
-            }
-            else{
-                wrong.push(guess); //adds the key press to the array called wrong
-                wrongBox.textContent = wrong;//I want the wrong answers to show
-            }
-        }
-    }
-}
+// document.onkeypress = function(input) {
+//     if(wrongLetters.indexOf(input.key)===-1){
+//         wrongBox.textContent = wrongLetters;
+//     }
+// }
