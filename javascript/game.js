69d8bfc527099c5b9f672f9a3e57241c5fcedf3c
Anderson <andersonswaimboyd@gmail.com>
2019-05-30 10:53:59 -0500

    game.js now works

--- javascript/game.js
+++ javascript/game.js
@@ -1,32 +1,50 @@
 //Defines our arrays
 var words = ["dog", "cat", "parrot", "goldfish", "hamster", "rabbit"];
 var wrongLetters = [];
-var answer = [];
-
-//defines random word from words array
-var randomWord = words[Math.floor(Math.random()*words.length)];
-var splits = randomWord.split("");
-
-var blanks = "_ ";
-var displayWord = blanks.repeat(randomWord.length);
-
-//where we want our game todisplay
+var letterBank = [];
+var emptyWord =[];
+var guesses = 10;
 var userGuess = document.getElementById("guess");
-var answerBox = document.getElementById("answer");
 var wrongBox = document.getElementById("wrongGuess");
 var display = document.getElementById("displayBlank");
 
-display.textContent = displayWord;
+//defines random word from words array
+var randomWord = words[Math.floor(Math.random()*words.length)];
+
+//creates blanks in blank array
+for(var i = 0; i < randomWord.length; i++){
+    emptyWord.push("_");
+}
+
+display.textContent = emptyWord;
 
 //guessing game itself (we need to compare the user input to the random array)
-document.onkeydown = function(event){
+document.onkeypress = function(event){
     userGuess.textContent = event.key;
-    if(wrongLetters.indexOf(event.key)!==-1){
-        answer.push(event.key);
-        display.textContent = answer.indexOf(event.key);  
-    }
-    else{
+    letterBank.push(event.key);
+    if(randomWord.indexOf(event.key)===-1){
         wrongLetters.push(event.key);
         wrongBox.textContent = wrongLetters;
+    }
+    if(letterBank.indexOf() === -1){
+        for(var x = 0; x < randomWord.length; x++){
+            if(randomWord[x] === event.key){
+                emptyWord[x] = event.key;
+                display.textContent = emptyWord;
+            }
+            else {
+                console.log("Not in array at input")
+            }
+        }
+        guesses--;
+        if(emptyWord.indexOf("_") === -1){
+            console.log("Winner!");
+        }
+        else if (guesses === 0){
+            console.log("Loser!");
+        }
+    }
+    else{
+        console.log("Already typed");
     }
 }
